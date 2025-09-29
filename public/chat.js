eae40d88ffa36f59b09a0beb674e669d257a1473
DiosGabriel <rodrgabriel842@gmail.com>
2025-09-29 12:13:00 -0600

    Update chat.js
    sonido no

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // ...existing code...
-var audioSend = new Audio('send.mp3');
+//var audioSend = new Audio('send.mp3');
 // ...existing code...
 var socket = io.connect('http://localhost:4000');
 var persona = document.getElementById('persona'),
